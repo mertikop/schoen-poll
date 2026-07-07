93bb3cb0e06835de1933132e40957d02d20af42c
Nicolas Boumal <nicolasboumal@gmail.com>
2026-07-07 21:31:47 +0200

    going back to original colors

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ export const firebaseConfig = {
 export const clickerUrl = "https://nicolasboumal.github.io/schoen-poll/";
 
 // Custom color palette for displaying the results (bubbles)
-// export const colors = ["#4C72B0", "#55A868", "#8172B2", "#64B5CD", "#CCB974", "#C44E52"];
-// export const colors = ["#02468E", "#007355", "#86228F", "#B88700", "#B63A4A", "#00869A"];
-export const colors = ["#02468E", "#007355", "#86228F", "#D55E00", "#B79F00", "#4E8CFF"];
+export const colors = ["#4C72B0", "#55A868", "#8172B2", "#64B5CD", "#CCB974", "#C44E52"];
+// export const colors = ["#02468E", "#007355", "#86228F", "#B88700", "#B63A4A", "#00869A"]; // goodnotes colors
+// export const colors = ["#02468E", "#007355", "#86228F", "#D55E00", "#B79F00", "#4E8CFF"]; // goodnotes colors
 
 // Define buttons with preset options
 export const presetButtons = [
