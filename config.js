d693ac0c3bd50a7aaf5cf85816a9998e2415a533
Panayotis Mertikopoulos <pmertikopoulos@gmail.com>
2026-07-08 17:44:01 +0200

    Personalization to mertikop firebase

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 // Copy/paste info from Firebase. Make sure to keep the word "export".
 export const firebaseConfig = {
-    apiKey: "AIzaSyAs9nhZuvHgGlPORPR3c_LYHenXjfbnamo",
-    authDomain: "schoen-poll.firebaseapp.com",
-    projectId: "schoen-poll",
-    storageBucket: "schoen-poll.firebasestorage.app",
-    messagingSenderId: "160493309944",
-    appId: "1:160493309944:web:40201dc27a1749c5a4500d"
+    apiKey: "AIzaSyD6KYmpnpvvyWiihSvgOCedZsRfNgYOwSw",
+    authDomain: "pingpoll-2e2bf.firebaseapp.com",
+    projectId: "pingpoll-2e2bf",
+    storageBucket: "pingpoll-2e2bf.firebasestorage.app",
+    messagingSenderId: "137681458818",
+    appId: "1:137681458818:web:8e55060443d0c7170f469b"
 };
 
 // Specify the URL to the clicker here, for the QR code.
-export const clickerUrl = "https://nicolasboumal.github.io/schoen-poll/";
+export const clickerUrl = "https://mertikop.github.io/schoen-poll/";
 
 // Custom color palette for displaying the results (bubbles)
 export const colors = ["#4C72B0", "#55A868", "#8172B2", "#64B5CD", "#CCB974", "#C44E52"];
